5a9d9697da558fea677d87894601e37da2790786
trickyzeeshan <tricky.zeeshan@gmail.com>
2018-01-25 13:35:08 +0530

    Refresh

--- app/src/main/java/com/example/jishan/languagesupport/MainActivity.java
+++ app/src/main/java/com/example/jishan/languagesupport/MainActivity.java
@@ -2,11 +2,8 @@ package com.example.jishan.languagesupport;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
